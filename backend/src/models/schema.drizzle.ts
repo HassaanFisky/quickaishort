93dae5a02df35555553ffc2a58a3a7339b368600
HassaanFisky <hassaanfisky@gmail.com>
2026-04-26 18:48:14 +0500

    chore: remove legacy frontend and backend folders to unify project on MongoDB Atlas

--- backend/src/models/schema.drizzle.ts
+++
@@ -1,7 +0,0 @@
-// This file is ONLY used by drizzle-kit (not the runtime server).
-// drizzle-kit runs in CJS mode so we use .ts imports resolved by tsx.
-export { users, subscriptionTierEnum } from "./user.model";
-export type { User, NewUser } from "./user.model";
-
-export { projects, projectStatusEnum } from "./project.model";
-export type { Project, NewProject } from "./project.model";
--- backend/src/models/schema.ts
+++
@@ -1,5 +0,0 @@
-export { users, subscriptionTierEnum } from "./user.model.js";
-export type { User, NewUser } from "./user.model.js";
-
-export { projects, projectStatusEnum } from "./project.model.js";
-export type { Project, NewProject } from "./project.model.js";
